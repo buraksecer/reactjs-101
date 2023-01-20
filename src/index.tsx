e9e84a5ba59cd055f03124f92e05a60607ce77b6
Burak Seçer <brkscr@hotmail.com.tr>
2023-01-21 00:21:04 +0300

    add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
-import App from './App';
+import App from './App-Router';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
